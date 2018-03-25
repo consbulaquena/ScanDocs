d24ad21e8cf948182785c5733c376fee9c2424d6
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-26 01:50:48 +0800

    Initial Commit

---
+++ ScanDocs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScanDocs
+//
+//  Created by Cons Bulaqueña on 26/03/2018.
+//  Copyright © 2018 consios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
