0d78572074015e00b3f17b589d38cc6e719a3973
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-26 02:19:27 +0800

    Add TesseractOCR iOS for Optical Character Recognition

---
+++ Pods/Target Support Files/Pods-ScanDocsTests/Pods-ScanDocsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScanDocsTests : NSObject
+@end
+@implementation PodsDummy_Pods_ScanDocsTests
+@end
---
+++ Pods/Target Support Files/Pods-ScanDocsUITests/Pods-ScanDocsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ScanDocsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ScanDocsUITests
+@end
