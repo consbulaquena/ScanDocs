40b79d30ab8252bbf71e1baba3af7e32eda0a355
Cons Bulaquena <consbulaquena@gmail.com>
2018-03-26 14:57:08 +0800

    Build user interface

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 [![Swift](https://img.shields.io/badge/Swift-4.0-orange.svg)]() [![Platform](https://img.shields.io/badge/platform-iOS-lightgrey.svg)]()
 
 
-This app can scan text accurately from images on your device (previously taken by your iPhone camera) or scan text from photos taken immediately by the camera.
+This app can scan text accurately from images on your device or scan text from photos taken immediately by the camera.
 
 
 ![alt tag]()
 
 ### Features
-<ul><li>Convert image to text from photos</li>
-<li>Used TesseractOCR iOS for Optical Character Recognition</li>
+<ul><li>Convert an image to text</li>
+<li>Edit the text, copy it to clipboard, and save in other apps</li>
+<li>Used <a href="https://github.com/gali8/Tesseract-OCR-iOS">TesseractOCR iOS</a> for Optical Character Recognition</li>
 <li>Access to iPhone's camera </li>
 <li>Deployment target: iOS 11.2+</li>
 <li>Supported devices: iPhone 5s, SE, 6, 6 Plus, 6s, 6s Plus, 7, 7 Plus, 8, 8 Plus, iPhone X </li>
--- ScanDocs/ViewController.swift
+++ ScanDocs/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+@IBDesignable extension UITextView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
 
+
+class ViewController: UIViewController {
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
-
+    
+    
 }
 
